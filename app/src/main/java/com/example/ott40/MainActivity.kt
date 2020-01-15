bcd6e759ee76a986747216a71fab62f6182a565a
Shlomo <shlomo.gevim@gmail.com>
2020-01-15 16:16:22 +0200

    second

--- app/src/main/java/com/example/ott40/MainActivity.kt
+++ app/src/main/java/com/example/ott40/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.ott40
 
+import android.animation.AnimatorInflater
 import android.graphics.drawable.AnimatedVectorDrawable
 import android.os.Bundle
 import android.util.Log
@@ -8,7 +9,6 @@ import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.*
 import kotlin.concurrent.fixedRateTimer
-import kotlin.coroutines.coroutineContext
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,21 +19,132 @@ class MainActivity : AppCompatActivity() {
     private lateinit var avd4: AnimatedVectorDrawable
     var counter = 0
 
+
+    private val coroutineScope = CoroutineScope(Dispatchers.Main)
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        preVision()
         prepareImageview(0)
-        prepare_avd()
-        actUpon()
+        coroutineScope.launch {
+            Log.d("clima", "contex->$coroutineContext")
+
+
+
+
+
+            prepare_avd1()
+
+            Log.d("clima", "contex1->$coroutineContext")
+            actUpon1()
+            Log.d("clima", "contex2->$coroutineContext")
+
+        }
+
+
+        /* prepareImageview(0)
+         prepare_avd()
+         actUpon()*/
     }
+
+    private fun preVision() {
+        imageView1.setImageResource(R.drawable.alleff)
+        imageView2.setImageResource(R.drawable.bet)
+        imageView3.setImageResource(R.drawable.gimel)
+        imageView4.setImageResource(R.drawable.dalet)
+
+
+    }
+
+    private suspend fun prepare_avd_to_animate() {
+        val avdd1 = coroutineScope.async(Dispatchers.Default) {
+            imageView1.drawable as AnimatedVectorDrawable
+        }
+
+        imageView2.setImageResource(R.drawable.bet)
+        val avdd2 =
+            coroutineScope.async(Dispatchers.Default) { imageView2.drawable as AnimatedVectorDrawable }
+
+        imageView3.setImageResource(R.drawable.gimel)
+        val avdd3 =
+            coroutineScope.async(Dispatchers.Default) { imageView3.drawable as AnimatedVectorDrawable }
+
+        val avdd4 =
+            coroutineScope.async(Dispatchers.Default) { imageView4.drawable as AnimatedVectorDrawable }
+    }
+
+    private fun actUpon1() {
+        prepareImageview(1)
+
+
+//        avd1.start()
+//        avd2.start()
+//        avd3.start()
+//        avd4.start()
+//        moveWord1()
+
+
+        /* GlobalScope.launch { draw_image1() }
+         GlobalScope.launch { draw_image2() }
+         GlobalScope.launch { draw_image3() }
+         GlobalScope.launch { draw_image4() }
+
+         GlobalScope.launch(Dispatchers.Main) { moveWord() }*/
+
+    }
+
+    private fun moveWord1() {
+        // delay(5000)
+        val anim = AnimatorInflater.loadAnimator(this, R.animator.set)
+        anim?.apply {
+            setTarget(word1Layout)
+            start()
+        }
+
+    }
+
+    private suspend fun prepare_avd1() {
+        imageView1.setImageResource(R.drawable.alleff)
+        val avdd1 = coroutineScope.async(Dispatchers.Default) {
+            imageView1.drawable as AnimatedVectorDrawable
+        }
+        val avd1 = avdd1.await()
+        imageView2.setImageResource(R.drawable.bet)
+        val avdd2 =
+            coroutineScope.async(Dispatchers.Default) { imageView2.drawable as AnimatedVectorDrawable }
+        val avd2 = avdd2.await()
+        imageView3.setImageResource(R.drawable.gimel)
+        val avdd3 =
+            coroutineScope.async(Dispatchers.Default) { imageView3.drawable as AnimatedVectorDrawable }
+        val avd3 = avdd3.await()
+        imageView4.setImageResource(R.drawable.dalet)
+        val avdd4 =
+            coroutineScope.async(Dispatchers.Default) { imageView4.drawable as AnimatedVectorDrawable }
+        val avd4 = avdd4.await()
+    }
+
 
     private fun actUpon() {
         prepareImageview(1)
+
         GlobalScope.launch { draw_image1() }
         GlobalScope.launch { draw_image2() }
         GlobalScope.launch { draw_image3() }
         GlobalScope.launch { draw_image4() }
 
+        GlobalScope.launch(Dispatchers.Main) { moveWord() }
+
+    }
+
+    private suspend fun moveWord() {
+        delay(5000)
+        val anim = AnimatorInflater.loadAnimator(this, R.animator.set)
+        anim?.apply {
+            setTarget(word1Layout)
+            start()
+        }
 
     }
 
@@ -45,58 +156,20 @@ class MainActivity : AppCompatActivity() {
         delay(1000)
         avd2.start()
     }
+
     private suspend fun draw_image3() {
         delay(2000)
         avd3.start()
     }
+
     private suspend fun draw_image4() {
         delay(3000)
         avd4.start()
     }
 
-
-
-    fun click_coroutine(view: View) {
-        prepare_avd()
-       // prepareImageview(1)
-        runBlocking {
-            launch {
-                delay(10)
-                imageView1.visibility = View.VISIBLE
-                avd1.start()
-                delay(2000)
-                imageView2.visibility = View.VISIBLE
-                avd2.start()
-            }
-            GlobalScope.launch(Dispatchers.Main) {
-                delay(4000)
-                imageView1.visibility = View.GONE
-                delay(1000)
-                imageView2.visibility = View.GONE
-
-            }
-
-
-
-
-
-        }
-
-    }
-
-
-    fun click_AllLetter(view: View) {
-        prepare_avd()
-        prepareImageview(1)
-
-        fixedRateTimer("timer", false, 0, 2000) {
-            counter++
-            operate_Animation(counter)
-            Log.d("clima", "counter=$counter")
-            if (counter >= 5) cancel()
-        }
-
-
+    private suspend fun removeWord() {
+        delay(6000)
+        word1Layout.visibility = View.INVISIBLE
     }
 
     private fun prepareImageview(ind: Int) {
@@ -123,6 +196,47 @@ class MainActivity : AppCompatActivity() {
         avd3 = imageView3.drawable as AnimatedVectorDrawable
         imageView4.setImageResource(R.drawable.dalet)
         avd4 = imageView4.drawable as AnimatedVectorDrawable
+    }
+
+
+    fun click_coroutine(view: View) {
+        prepare_avd()
+        // prepareImageview(1)
+        runBlocking {
+            launch {
+                delay(10)
+                imageView1.visibility = View.VISIBLE
+                avd1.start()
+                delay(2000)
+                imageView2.visibility = View.VISIBLE
+                avd2.start()
+            }
+            GlobalScope.launch(Dispatchers.Main) {
+                delay(4000)
+                imageView1.visibility = View.GONE
+                delay(1000)
+                imageView2.visibility = View.GONE
+
+            }
+
+
+        }
+
+    }
+
+
+    fun click_AllLetter(view: View) {
+        prepare_avd()
+        prepareImageview(1)
+
+        fixedRateTimer("timer", false, 0, 2000) {
+            counter++
+            operate_Animation(counter)
+            Log.d("clima", "counter=$counter")
+            if (counter >= 5) cancel()
+        }
+
+
     }
 
 
